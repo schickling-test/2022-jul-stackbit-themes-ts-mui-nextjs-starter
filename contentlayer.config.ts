e0b37d1e24687173449c525daf0f41950c14928c
Johannes Schickling <schickling.j@gmail.com>
2022-09-09 18:31:15 +0200

    new api

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,24 +1,25 @@
-// NOTE the APIs in this file are an early draft and should evolve based on feedback
 import { makeSource } from 'contentlayer/source-files'
-import { addComputedFields, stackbitConfigToDocumentTypes } from '@contentlayer/experimental-source-files-stackbit'
+import { stackbitConfigToDocumentTypes } from '@contentlayer/experimental-source-files-stackbit'
 import stackbitConfig from './stackbit.config.js'
 
-const documentTypes = stackbitConfigToDocumentTypes(stackbitConfig)
+const documentTypes = stackbitConfigToDocumentTypes(stackbitConfig, {
+  documentTypes: {
+    Page: {
+      computedFields: {
+        slug: { type: 'string', resolve: (_) => _._raw.flattenedPath.replace(/^pages\/?/, '/') },
+        __id: { type: 'string', resolve: (_) => 'content/' + _._id },
+      },
+    },
 
-addComputedFields({
-  documentTypes,
-  documentTypeName: 'Page',
-  computedFields: {
-    slug: { type: 'string', resolve: (_) => _._raw.flattenedPath.replace(/^pages\/?/, '/') },
-    __id: { type: 'string', resolve: (_) => 'content/' + _._id },
+    Config: {
+      computedFields: {
+        __id: { type: 'string', resolve: (_) => 'content/' + _._id },
+      },
+      fields: { favicon: { type: 'string' } },
+    },
   },
-})
-
-addComputedFields({
-  documentTypes,
-  documentTypeName: 'Config',
-  computedFields: {
-    __id: { type: 'string', resolve: (_) => 'content/' + _._id },
+  nestedTypes: {
+    Image: { fields: { url: { type: 'string' } } },
   },
 })
 
